8ba316ed9a2e64133b0bb91a3f34f82def00d0de
Romain Torrente <romain.torrente@insa-lyon.fr>
2018-11-02 20:25:40 +0100

    Change main url

--- mapp/urls.py
+++ mapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('members/', include('active_members.urls')),
+    path('', include('active_members.urls')),
 ]
