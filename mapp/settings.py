53046ba00d4c174f07b01bb7057c4867b4a27e3e
rfournier1 <romain.fournier@insa-lyon.fr>
2019-04-06 11:51:36 +0200

    LOL

--- mapp/settings.py
+++ mapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'active_members',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
